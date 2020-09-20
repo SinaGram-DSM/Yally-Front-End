aaac8d8d770398efaa7ed6717b0efe934c4bdbe1
Haerin <kim34458747@gmail.com>
2020-09-20 23:40:08 +0900

    Myprofile UI과 style 병합

---
+++ yally/src/assets/style/Main/AddTimeLine.js
@@ -0,0 +1,78 @@
+import styled from 'styled-components'
+
+export const mainContainer = styled.div`
+    display: flex;
+    margin: ${props => props.profile? '0px 392px 0px 392px' : '0px 392px 50px 392px'};
+    justify-content : space-between;
+    
+`;
+
+export const mainSection  = styled.section`
+    width : 100%;
+    background-color : #ffffff;
+    box-shadow: ${props => props.profile? 'none' : '0 0 6px #00000004'};
+    padding :  ${props => props.profile? 'none' : '40px'};
+    margin-top: ${props => props.feed? 'none' : '60px'};
+`;
+
+export const writerInfoBox = styled.div`
+    height: 106px;
+    display: flex;
+    align-items: center;
+    border-bottom: ${props=>props.profile? 'none' : '1px solid #EFEFEF' };
+    margin : 0px 0px 25px 0px;
+    padding-bottom : 20px;
+`;
+
+export const form = styled.form`
+    width : 100%;
+`;
+
+export const writerInput = styled.input`
+    width: 95%;
+    height: 100px;
+    margin-left: 15px;
+    border: none;
+    font-size: 25px;
+    font-weight: 300;
+`;
+
+export const profileImg = styled.img`
+    width: ${props => props.profile ? '130px': '70px' };
+    height:  ${props => props.profile ? '130px': '70px' };
+    border-radius: 9999px;
+    border: none;
+    background-color: rgb(211, 183, 183);
+    box-shadow : 0 0 6px #00000016;
+`;
+
+export const buttonsContainer = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const buttonBox = styled.button`
+    width: 102px;
+    height: 43px;
+    background-color: #EFEFEF;
+    border-radius: 23px;
+    margin-right: 20px;
+    border: none;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    outline: none;
+    cursor: pointer;
+    color: #707070;
+    font-size: 15px;
+    padding: 10px;
+    &:hover {
+        background-color : #D1D1D1;
+        transition : 0.3s
+    }
+`;
+
+export const buttonIcon = styled.img`
+    width : 16px;
+    height : 16px;
+`;
---
+++ yally/src/assets/style/UserPage/PageStyle.js
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+width: 55%;
+height: auto;
+margin: 0 auto;
+position: relative;
+` 
+export const Profile = styled.div`
+width: 100%;
+height: 200px;
+border: 1px solid #B9B9B9;
+padding: 50px 0px 0px 0px;
+line-height: 140px;
+`
+export const backgroundBox = styled.img`
+  position: absolute;
+  z-index: 0;
+  width: 100%;
+  height: 80%;
+`
+
+export const FeedContainer = styled.div`
+    width: 100%;
+    height: 400px;
+    border: 1px solid #B9B9B9;
+`
+
+export const ProfileImg = styled.div`
+    width: 150px;
+    height: 150px;
+    display: inline-block;
+    float: left;
+    border-radius: 50%;
+    box-shadow: 0px 0px 20px 3px hsl(0, 0%, 0%, 0.13);
+    overflow: hidden;
+`
+export const Img = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+
+export const UserName = styled.span`
+    color: #707070;
+    font-size: 30px;
+    vertical-align: middle;
+    margin-left: 30px;
+`
+export const Email = styled(UserName)`
+    font-size: 30px;
+    margin-left: 5px;
+`
+
+export const ProfileData = styled.div`
+    marign: 10px;
+`
+export const Listen = styled.div`
+    margin: 20px 0px 0px 10px   ;    
+`
+export const Listening = styled.span`
+    color: #707070;
+    font-size: 23px;
+    margin: 0px 20px 0px 30px;
+`
+
+export const Listener = styled(Listening)`
+`
