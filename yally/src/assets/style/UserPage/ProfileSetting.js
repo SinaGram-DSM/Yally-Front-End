854f251f357f638c359fad74e764dc7d50a93f22
Haerin <kim34458747@gmail.com>
2020-09-29 02:30:01 +0900

    profile setting UI

--- yally/src/assets/style/UserPage/ProfileSetting.js
+++ yally/src/assets/style/UserPage/ProfileSetting.js
@@ -1,2 +1,94 @@
 import styled from 'styled-components';
 
+export const settingContainer =  styled.div`
+    display: flex;
+    margin: 0px 392px 0px 392px;
+
+`
+
+export const settingSection = styled.section`
+    width: 100%;
+    padding: 60px 190px 0px 190px;
+    
+`
+
+export const topSection = styled.section`
+    text-align: center;
+`
+
+export const title = styled.p`
+    color: #707070;
+    font-size: 35px;
+    font-weight: 400;
+    margin: 10px;
+`
+export const headerBorder = styled.div`
+    width: 90%;
+    height: 5px;
+    border: none; 
+    background: linear-gradient(to right, #6E8EEA, #9B78EC);
+    margin: 0 auto;
+    
+`
+
+
+export const profileSection = styled.section`
+    width: 28%;
+    margin: 60px auto;   
+`
+export const imgInput = styled.input.attrs({
+    type: 'file',
+})`
+    display: none;
+`
+
+export const imgChange = styled.label.attrs({
+    for: 'input-img'
+})`
+   cursor: pointer; 
+`
+
+export const imgBox = styled.div`
+    width: 120px;
+    height: 120px;
+    position: relative;
+    line-height: 110px;
+
+`
+export const profileChange = styled.img`
+    width: 70%;
+    position: relative;
+    top: 0;
+    left: 17px;
+    z-index: 2;
+    vertical-align: middle;
+`
+export const profileImge = styled.img`
+    width: 100%;
+    position: absolute;
+    left: 0;
+    right: 0;
+    z-index: 1;
+    filter: brightness(50%);
+
+`
+export const nameBox = styled.div`
+    width: 70%;
+    margin: 0 auto;
+`
+export const nickname = styled.input.attrs({
+    type: 'text'
+})`
+    width: 100%;
+    color: #707070;
+    border: none;
+    border-bottom: 1px solid #D1D1D1;
+    font-size: 15px;
+    margin: 0 auto;
+    outline: none;
+    ::placeholder {
+        color: #707070;
+    }
+
+`
+
--- yally/src/components/AccountSetting/Setting.js
+++ yally/src/components/AccountSetting/Setting.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import * as P from '../../assets/style/UserPage/ProfileSetting'
 import * as M from '../../assets/style/Main/AddTimeLine';
+import {test, profileEdit} from '../../assets/img';
+
+
 const Setting = () => {
     return(
-        <M.mainContainer>
-            
-        </M.mainContainer>
+        <P.settingContainer>
+            <P.settingSection>
+                <P.topSection>
+                    <P.title>Account Setting</P.title>
+                    <P.headerBorder></P.headerBorder>
+                </P.topSection>
+                <P.profileSection>
+                        <P.imgInput id="input-img"></P.imgInput>
+                        <P.imgChange>
+                            <P.imgBox>
+                                <P.profileChange src={profileEdit} />
+                                <P.profileImge src={test}/>
+                            </P.imgBox>
+                        </P.imgChange>
+                </P.profileSection>
+                <P.nameBox>
+                    <P.nickname placeholder="데인드한" onfocus=""/>
+                </P.nameBox>
+                
+            </P.settingSection>
+        </P.settingContainer>
     );
 }
 
