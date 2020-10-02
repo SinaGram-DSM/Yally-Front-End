50a604b87744081c08d5ecf04fc60d613b8309dc
Srim <deamoon0801@naver.com>
2020-10-03 00:29:45 +0900

    [ADD] 댓글 삭제

--- yally/src/components/PostDetail/Comment.js
+++ yally/src/components/PostDetail/Comment.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import * as S from "../../assets/style/Main/AddTimeLine";
 import * as C from '../../assets/style/PostDetail/Comment'
+import * as P from "../../assets/style/Main/PostItmes"
+import { deleteIcon } from '../../assets/img'
+import axios from 'axios'
 
-const Comment = ({src, sound, nickname, date, content, userImg}) => {
-    console.log(content)
+const Comment = ({id, baseUrl, src, sound, nickname, date, content, userImg, deleteButtonStyle}) => {
+    let soundStyle;
+    const config = {
+        headers : { 'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8'}
+    }
+
+    if(sound == null)
+    {
+        soundStyle = 'none';
+    }
+
+    const onRemoveComment = async () => {
+        await axios.delete(baseUrl + "post/comment/" + id, config)
+        setTimeout(function() {
+            window.location.reload();
+          }, 300);
+    }
+
     return (
             <S.mainContainer detailPost>
             <S.mainSection small>
@@ -13,7 +32,9 @@ const Comment = ({src, sound, nickname, date, content, userImg}) => {
                         <C.commentInfo>
                             <C.commentWriter>{nickname}</C.commentWriter>
                             <C.commentDate>{date}</C.commentDate>
+                            <P.Icon comment src={deleteIcon} style={{display : deleteButtonStyle}} onClick={onRemoveComment}></P.Icon>
                         </C.commentInfo>
+                        <audio src={sound} style={{display : soundStyle}} controls></audio>
                         <C.contents>{content}</C.contents>
                     </C.div>
                 </C.commentBox>
