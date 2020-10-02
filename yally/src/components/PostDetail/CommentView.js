6e211f7f4e840cd401cf032400be65146591c4d7
Srim <deamoon0801@naver.com>
2020-10-03 00:29:29 +0900

    [ADD] Comment View

--- yally/src/components/PostDetail/CommentView.js
+++ yally/src/components/PostDetail/CommentView.js
@@ -5,13 +5,12 @@ import Comment from './Comment'
 import AddComment from './AddComment';
 import axios from 'axios';
 
-const CommentView = ({src, baseUrl, id}) => {
+const CommentView = ({src, baseUrl, id, deleteButtonStyle}) => {
     const [comment, setComment] = useState([]);
     const config = {
         headers : { 'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8'}
     }
 
-    console.log(id)
     useEffect(() => { 
         axios.get(baseUrl + "post/" + id + "/comment", config)
         .then((res) => {
@@ -23,13 +22,16 @@ const CommentView = ({src, baseUrl, id}) => {
                 <C.commentContainer>
                     {comment.map((c) => (
                         <Comment
+                        deleteButtonStyle = {deleteButtonStyle}
                         src = {src}
+                        id = {c.id}
                         key={c.id}
                         content = {c.content}
                         sound = {c.sound}
                         date = {c.createdAt}
                         nickname = {c.user.nickname}
                         userImg = {c.user.img}
+                        baseUrl = {baseUrl}
                         ></Comment>
                     ))}
                     <AddComment baseUrl={baseUrl} id={id}></AddComment>
