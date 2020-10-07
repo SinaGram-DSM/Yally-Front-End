e678fbfe686741c796f8186020726d4c9c0827a7
Haerin <kim34458747@gmail.com>
2020-10-07 19:55:48 +0900

    profile 서버 통신

--- yally/src/components/UserPage/MyPage.js
+++ yally/src/components/UserPage/MyPage.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as M from '../../assets/style/UserPage/PageStyle';
-import * as S from '../../assets/style/Main/AddTimeLine'
+import * as S from '../../assets/style/Main/AddTimeLine';
+import axios from 'axios';
 import MyFeed from './MyFeed';
 
 const MyPage = () => {
+    
+    let [name, setName] = useState('');
+    // let [image, setImage] = useState('');
+    let [data, setData] = useState({
+        listening: 0,
+        listener: 0
+    });
+    
+    useEffect = (() => {
+        getProfile();
+    });
+
+    const config = {
+        headers : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8'
+    }
+
+    const getProfile = () => {
+        axios.get("http://13.125.238.84:81/profile/", config)
+        .than((res) => {
+            setData({
+                ...data,
+                listening: res.data.listening, 
+                listener: res.data.listener
+            });
+            setName(res.data.nickname);
+        })
+    }
+
+    
     return(
         <div>
         <S.mainContainer profile>
@@ -11,11 +41,11 @@ const MyPage = () => {
             <S.writerInfoBox profile>
                 <S.profileImg profile></S.profileImg>
             <M.ProfileData>
-                <M.UserName>데인 드한</M.UserName>   
+                <M.UserName>{name}</M.UserName>   
                 <M.Email>(dehaan@hansome.kr)</M.Email>
             <M.Listen>
-                <M.Listening>리스닝 123</M.Listening>
-                <M.Listener>리스너 123</M.Listener>
+                <M.Listening>{data.listening}</M.Listening>
+                <M.Listener>{data.listener}</M.Listener>
                 </M.Listen>
                 </M.ProfileData>
             </S.writerInfoBox>
