90c6ea01e244d90e16fe26cfb8a40eb0f34c9df8
Haerin <kim34458747@gmail.com>
2020-10-24 00:35:59 +0900

    listen list

--- yally/src/components/Listen/Listener.js
+++ yally/src/components/Listen/Listener.js
@@ -4,72 +4,54 @@ import * as T from '../../assets/style/UserPage/Listen';
 import * as L from '../../assets/style/UserPage/PageStyle';
 import * as M from '../../assets/style/Main/AddTimeLine';
 
-const Listener = (baseUrl) => {
-    let [listener, setListener] = useState({
-        nickname: "",
-        listenerValue: 0,
-        listening: 0,
-        isListening: true 
-    });
-
-    // const email = this.props.match; 라우터 주소에서 이메일 가져오기 
+const Listener = ({baseUrl, match}) => {
+    let [listeners, setListeners] = useState([]);
+    // const email = this.props.match; 라우터 주소에서 이메일 가져오기
     
-
+    const listenerValue = match.match.params.value;
+    const name = match.match.params.name;
     const config = {
         headers: {
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8'
+            'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDM0NDA5MjksIm5iZiI6MTYwMzQ0MDkyOSwianRpIjoiYWY5MThlYmItZjg3NC00YjVkLWE2NmMtNGI0Njg1ZTdmNjJjIiwiZXhwIjoxNjEyMDgwOTI5LCJpZGVudGl0eSI6ImFkbWluMTIzQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.qxlO5x1-cGIOkNsHgu53shHxjFZl-yVcAI8G7xtk4l4'
         }
     }
 
     useEffect (() => {
-        
-        axios.get(baseUrl + "/profile/admin123@gmail.com", config)
+        axios.get("http://13.125.238.84:81/profile/admin123@gmail.com/listener", config)
         .then((res) => {
-            setListener({
-                ...listener, 
-                nickname: res.data.listeners.nickname, 
-                listenerValue: res.data.listeners.listener,
-                listening: res.data.listeners.listening,
-                isListening: res.data.listeners.isListening
-            })
-            console.log(res.data.listeners);
+            setListeners(res.data.listeners)
         })
-    }) 
-    return(
+        console.log(match);
+    }, []) 
 
+    return(
         <M.mainContainer>
         <T.mainSection>
         <T.profileSection>
         <T.profileImg></T.profileImg>
-        <T.comment>{listener.listenerValue}명이 마멜공주 님의 이야기를 듣고 있습니다.</T.comment>
+        <T.comment>{listenerValue}명이 {name} 님의 이야기를 듣고 있습니다.</T.comment>
         </T.profileSection>
         <T.listenSection>
+        {listeners.map((listener) => (
             <T.containerBox>
                 <T.itemBox>
                   <T.boxImg/>  
                 <T.userBox>
                     <T.name>{listener.nickname}</T.name>
                     <L.Listen list>
-                        <L.Listening listen>리스닝 {listener.listenerValue}</L.Listening>
-                        <L.Listener listen>리스너 {listener.listening}</L.Listener>
+                        <L.Listening listen>리스닝 {listeners.listening}</L.Listening>
+                        <L.Listener listen>리스너 {listeners.listener}</L.Listener>
                     </L.Listen>
                 </T.userBox>
                 
                 </T.itemBox>
-                {listener.isListening ? 
+                {listeners.isListening ? 
                 <T.listenBtn>리스닝</T.listenBtn> :
                 <T.unlistenBtn>언리스닝</T.unlistenBtn> }
             </T.containerBox>
-            <T.containerBox>
-                
-                </T.containerBox>
-                
-            <T.containerBox>
-                
-                </T.containerBox>
       
-                
-        </T.listenSection>
+        ))}
+          </T.listenSection>
         </T.mainSection>
     </M.mainContainer>
     )
--- yally/src/components/Listen/Listening.js
+++ yally/src/components/Listen/Listening.js
@@ -4,67 +4,53 @@ import * as T from '../../assets/style/UserPage/Listen';
 import * as L from '../../assets/style/UserPage/PageStyle';
 import * as M from '../../assets/style/Main/AddTimeLine';
 
-const Listening = () => {
+const Listening = ({match}) => {
     const isListen = false; //리슨 버튼 언리스닝 리스닝 여부 확인 값
-    let [listening, setListening] = useState({
-        nickname: "",
-        listener: 0,
-        listeningValue: 0,
-        isListening: true 
-    });
+    let [listenings, setListenings] = useState([]);
 
+    const listeningValue = match.match.params.value;
+    const name = match.match.params.name;
+    
     const config = {
         headers: {
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8'
+            'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8'
         }
     }
 
     useEffect(() => {
-        axios.get("http://13.125.238.84:81/profile/admin123@gmail.com/", config)
+        axios.get("http://13.125.238.84:81/profile/admin123@gmail.com/listening", config)
         .then((res) => {
-            setListening({
-                ...listening,
-                nickname: res.data.listener.nickname,
-                listener: res.data.listeners.listener,
-                listeningValue: res.data.listeners.listening,
-                isListening: res.data.listeners.isListening
-            })
-            console.log(res.data);
+            setListenings(res.data.listenings)
         })
-    })
+    }, [])
+
+
     return(
         <M.mainContainer>
         <T.mainSection>
         <T.profileSection>
         <T.profileImg></T.profileImg>
-        <T.comment>마멜공주 님이 {listening.listeningValue}명의 이야기를 듣고 있습니다.</T.comment>
+        <T.comment>{name} 님이 {listeningValue}명의 이야기를 듣고 있습니다.</T.comment>
         </T.profileSection>
         <T.listenSection>
-        <T.containerBox>
-            <T.itemBox>
-              <T.boxImg/>  
-            <T.userBox>
-                <T.name>{listening.nickname}</T.name>
-                <L.Listen list>
-                    <L.Listening listen>리스닝{listening.listeningValue}</L.Listening>
-                    <L.Listener listen>리스너 {listening.listener}</L.Listener>
-                </L.Listen>
-            </T.userBox>
-            </T.itemBox>
-            {isListen ? 
-                <T.listenBtn>리스닝</T.listenBtn> :
-                <T.unlistenBtn>언리스닝</T.unlistenBtn> }
-            
-        </T.containerBox>
-        <T.containerBox>
-            
-            </T.containerBox>
-            
-        <T.containerBox>
-            
-            </T.containerBox>
-  
-            
+        {listenings.map((listening) => (
+             <T.containerBox>
+             <T.itemBox>
+               <T.boxImg/>  
+             <T.userBox>
+                 <T.name>{listening.nickname}</T.name>
+                 <L.Listen list>
+                     <L.Listening listen>리스닝{listening.listeningValue}</L.Listening>
+                     <L.Listener listen>리스너 {listening.listener}</L.Listener>
+                 </L.Listen>
+             </T.userBox>
+             </T.itemBox>
+             {isListen ? 
+                 <T.listenBtn>리스닝</T.listenBtn> :
+                 <T.unlistenBtn>언리스닝</T.unlistenBtn> }
+             
+         </T.containerBox>
+        ))} 
     </T.listenSection>
     </T.mainSection>
     </M.mainContainer>
