ddc9ef70da1197c6cd8de6c209a4a8f77cbd17bd
Haerin <kim34458747@gmail.com>
2020-10-23 16:16:15 +0900

    Listen 서버 통신

--- yally/src/components/Listen/Listener.js
+++ yally/src/components/Listen/Listener.js
@@ -1,24 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import * as T from '../../assets/style/UserPage/Listen';
 import * as L from '../../assets/style/UserPage/PageStyle';
+import * as M from '../../assets/style/Main/AddTimeLine';
 
-const Listener = () => {
-    const isListen = true;
+const Listener = (baseUrl) => {
+    let [listener, setListener] = useState({
+        nickname: "",
+        listenerValue: 0,
+        listening: 0,
+        isListening: true 
+    });
+
+    // const email = this.props.match; 라우터 주소에서 이메일 가져오기 
+    
+
+    const config = {
+        headers: {
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8'
+        }
+    }
+
+    useEffect (() => {
+        
+        axios.get(baseUrl + "/profile/admin123@gmail.com", config)
+        .then((res) => {
+            setListener({
+                ...listener, 
+                nickname: res.data.listeners.nickname, 
+                listenerValue: res.data.listeners.listener,
+                listening: res.data.listeners.listening,
+                isListening: res.data.listeners.isListening
+            })
+            console.log(res.data.listeners);
+        })
+    }) 
     return(
+
+        <M.mainContainer>
+        <T.mainSection>
+        <T.profileSection>
+        <T.profileImg></T.profileImg>
+        <T.comment>{listener.listenerValue}명이 마멜공주 님의 이야기를 듣고 있습니다.</T.comment>
+        </T.profileSection>
         <T.listenSection>
             <T.containerBox>
                 <T.itemBox>
                   <T.boxImg/>  
                 <T.userBox>
-                    <T.name>최수빈</T.name>
+                    <T.name>{listener.nickname}</T.name>
                     <L.Listen list>
-                        <L.Listening listen>리스닝 123</L.Listening>
-                        <L.Listener listen>리스너 456</L.Listener>
+                        <L.Listening listen>리스닝 {listener.listenerValue}</L.Listening>
+                        <L.Listener listen>리스너 {listener.listening}</L.Listener>
                     </L.Listen>
                 </T.userBox>
                 
                 </T.itemBox>
-                {isListen ? 
+                {listener.isListening ? 
                 <T.listenBtn>리스닝</T.listenBtn> :
                 <T.unlistenBtn>언리스닝</T.unlistenBtn> }
             </T.containerBox>
@@ -32,6 +70,8 @@ const Listener = () => {
       
                 
         </T.listenSection>
+        </T.mainSection>
+    </M.mainContainer>
     )
 }
 
--- yally/src/components/Listen/Listening.js
+++ yally/src/components/Listen/Listening.js
@@ -1,19 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import * as T from '../../assets/style/UserPage/Listen';
 import * as L from '../../assets/style/UserPage/PageStyle';
+import * as M from '../../assets/style/Main/AddTimeLine';
 
 const Listening = () => {
     const isListen = false; //리슨 버튼 언리스닝 리스닝 여부 확인 값
+    let [listening, setListening] = useState({
+        nickname: "",
+        listener: 0,
+        listeningValue: 0,
+        isListening: true 
+    });
+
+    const config = {
+        headers: {
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8'
+        }
+    }
+
+    useEffect(() => {
+        axios.get("http://13.125.238.84:81/profile/admin123@gmail.com/", config)
+        .then((res) => {
+            setListening({
+                ...listening,
+                nickname: res.data.listener.nickname,
+                listener: res.data.listeners.listener,
+                listeningValue: res.data.listeners.listening,
+                isListening: res.data.listeners.isListening
+            })
+            console.log(res.data);
+        })
+    })
     return(
+        <M.mainContainer>
+        <T.mainSection>
+        <T.profileSection>
+        <T.profileImg></T.profileImg>
+        <T.comment>마멜공주 님이 {listening.listeningValue}명의 이야기를 듣고 있습니다.</T.comment>
+        </T.profileSection>
         <T.listenSection>
         <T.containerBox>
             <T.itemBox>
               <T.boxImg/>  
             <T.userBox>
-                <T.name>투바투</T.name>
+                <T.name>{listening.nickname}</T.name>
                 <L.Listen list>
-                    <L.Listening listen>리스닝 456</L.Listening>
-                    <L.Listener listen>리스너 123</L.Listener>
+                    <L.Listening listen>리스닝{listening.listeningValue}</L.Listening>
+                    <L.Listener listen>리스너 {listening.listener}</L.Listener>
                 </L.Listen>
             </T.userBox>
             </T.itemBox>
@@ -32,6 +66,8 @@ const Listening = () => {
   
             
     </T.listenSection>
+    </T.mainSection>
+    </M.mainContainer>
     );
 }
 
