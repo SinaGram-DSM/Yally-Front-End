b5a8d3d25fcd96ba1189c268d723d35dc4b71e9e
성동명 <sungdm0303@gmail.com>
2020-10-26 16:48:48 +0900

    signup

--- yally/src/assets/style/SignUp/SignUpCheckForm.js
+++ yally/src/assets/style/SignUp/SignUpCheckForm.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import * as L from './Login';
+import * as L from './login';
 
 export const input = styled(L.input)`
     margin-bottom: 200px;
@@ -11,4 +11,28 @@ export const button = styled(L.loginButton)`
 
 export const info = styled(L.greet)`
     margin-bottom: 50px;
+`;
+
+export const help = styled.img`
+    height: 20px;
+    width: 20px;
+`;
+
+export const helpBox = styled(L.mainContainer)`
+    position: absolute;
+    border: 1px solid #D1D1D1;
+    width: 320px;
+    top: 70px;
+    left: 520px;
+`;
+
+export const QA = styled.span`
+    color: #6665E7;
+`;
+
+export const helpText = styled.p`
+    color: #707070;
+    font-size: 16px;
+    padding-right: 5px;
+    padding-left: 5px;
 `;
