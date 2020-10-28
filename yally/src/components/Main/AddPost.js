2887c03713d7e748ed00b6bfb59e7cc9f29d9169
Srim <deamoon0801@naver.com>
2020-10-28 19:10:28 +0900

    [ADD] post edit

--- yally/src/components/Main/AddPost.js
+++ yally/src/components/Main/AddPost.js
@@ -1,16 +1,12 @@
 import React,{ useState, useRef } from 'react';
 import * as S from "../../assets/style/Main/AddTimeLine";
 import * as R from '../../assets/style/Main/Recommend';
-import { mic, sound, picture } from '../../assets/img';
+import { sound, picture } from '../../assets/img';
+import AudioRecord from './AudioRecord'
 import axios from 'axios';
 
-const AddPost = ({src, baseUrl, userImg}) => {
+const AddPost = ({src, baseUrl, userImg, editContent, editFile, editImg, editPostId}) => {
 
-    const [stream, setStream] = useState({});
-    const [media, setMedia] = useState({});
-    const [onRec, setOnRec] = useState(true);
-    const [source, setSource] = useState({});
-    const [analyser , setAnalyser] = useState({});
     const [audioUrl, setAudioUrl] = useState();
     const [previewUrl, setPreviewUrl] = useState('');
     const [imgFile, setImgFile] = useState('');
@@ -19,92 +15,24 @@ const AddPost = ({src, baseUrl, userImg}) => {
     const [start, setStart] = useState();
     const [end, setEnd] = useState();
     const [onText, setOnText] = useState();
+    let a = false;
     let recArr = [];
     let imgPreview = null;
     let recPreview = null;
-
-    const postInput = useRef();
-
-    const onRecAudio = () => {
-        let rsec = 1;
-        let lsec = 0;
-        let rmin = 0;
-        let timer = setInterval(() => {
-            setOnText(<S.buttonsContainer><S.recordingIcon></S.recordingIcon><S.recordText>{'0' + rmin + ' : ' + lsec + rsec}</S.recordText></S.buttonsContainer>);
-            setOnRecText(<S.recordText>버튼을 누르면 녹음이 종료됩니다.</S.recordText>);
-            rsec++;
-            if(rsec > 9) 
-            {
-                lsec++;
-                rsec = 0;
-            }
-            if(lsec > 5) 
-            {
-                lsec = 0;
-                rmin++;
-            }
-        }, 1000)
-        setStart(timer);
-        const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
-        const analyser = audioCtx.createScriptProcessor(0,1,1);
-        setAnalyser(analyser);
-        
-        function makeSound(stream) {
-            const source = audioCtx.createMediaStreamSource(stream);
-            setSource(source)
-            source.connect(analyser);
-            analyser.connect(audioCtx.destination);
+    const [h, setH] = useState(false);
+    const config = {
+        headers : { 'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8',
+        'Content-type': 'application/x-www-form-urlencoded'
         }
-
-        navigator.mediaDevices.getUserMedia({ audio: true })
-            .then(stream => {
-            const mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.start();
-            setStream(stream);
-            setMedia(mediaRecorder);
-            makeSound(stream);
-            
-            analyser.onaudioprocess = function(e) {
-                if(e.playbackTime > 300)
-                {
-                    stream.getAudioTracks().forEach(function(track) {
-                        track.stop();
-                    });
-                    mediaRecorder.stop();
-                    analyser.disconnect();
-                    audioCtx.createMediaStreamSource(stream).disconnect();
-                    
-                    mediaRecorder.ondataavailable = function(e) {
-                        recArr.push(e.data);
-                        setAudioUrl(recArr[0]);
-                        
-                        setOnRec(true);
-                    }
-                }
-                else
-                {
-                    setOnRec(false)
-                }
-            }
-            
-        })
     }
-    const offRecAudio = () => {
-        media.ondataavailable = function(e) {
-            recArr.push(e.data);
-            setAudioUrl(recArr[0]);
-        }
-        stream.getAudioTracks().forEach(function(track) {
-            track.stop();
-        });
-        media.stop();
-        
-        analyser.disconnect();
-        source.disconnect(); 
-        setIsOnAudio(true);
-        setOnRecText(null);
-        clearInterval(start);
-        setOnText(null);
+
+    const setRecord = (audio, onAudio) => {
+        setAudioUrl(audio);
+        setIsOnAudio(onAudio);
+    }
+
+    const setTimer = (isOnRec) => {
+       setStart(isOnRec);
     }
 
     const onUploadRec = () => {
@@ -113,14 +41,47 @@ const AddPost = ({src, baseUrl, userImg}) => {
         setIsOnAudio(true);
     }
 
+    const onEditPost = () => {
+        
+        let a = [];
+        let b = [];
+        const ii = document.getElementById("audioImg").files;
+        a.push(editFile);
+        b.push(editImg);
+        // const i = new File([b], "img", {type : 'img/jpeg'});
+        const sound = new File([a], "sound", { lastModified: new Date().getTime(), type: 'audio/mp3' });
+        const img = new File([b], "img", { lastModified: new Date().getTime(), type: 'img/jpeg' });
+        // console.log(sound);
+        // let editSound = new File([audioUrl], "soundBlob",{ lastModified: new Date().getTime(), type: audioUrl.type });
+        
+        const editFormData = {
+            sound : sound,
+            content : editContent,
+            img : img,
+            hashtags : 'zz'
+        };
+        console.log(editFormData);
+        let editForm = new FormData();
+        editForm.append('content', editFormData.content);
+        editForm.append('img', editFormData.img);
+        editForm.append('sound', editFormData.sound);
+        editForm.append('hashtag', editFormData.content);
+
+        console.log(editForm);
+        axios.post(baseUrl + "post/" + editPostId, editForm, config)
+            .then((res) => {
+                console.log(res);
+                setTimeout(function() {
+                    window.location.reload();
+                }, 200);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
     const onAddPost = () => {
-
-        const config = {
-            headers : { 'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8',
-            'Content-type': 'application/x-www-form-urlencoded'
-            }
-        }
-
+        
         const content = document.getElementsByName('content')[0].value.trim();
         const img = document.getElementById("audioImg").files;
         let hashtagArr = [];
@@ -139,6 +100,7 @@ const AddPost = ({src, baseUrl, userImg}) => {
         if(audioUrl == null)
         {
             alert('음성을 녹음하거나 업로드해주세요!');
+            console.log(audioUrl)
         }
         
         else if(typeof(audioUrl) == FileList)
@@ -150,6 +112,7 @@ const AddPost = ({src, baseUrl, userImg}) => {
                 file : img,
                 hashtag : hashtagArr
             };
+            console.log(audioUrl)
 
             form.append('content', formdata.content);
             form.append('img', formdata.file[0]);
@@ -164,7 +127,7 @@ const AddPost = ({src, baseUrl, userImg}) => {
                 console.log(res)
                 setTimeout(function() {
                     window.location.reload();
-                }, 300);
+                }, 200);
             })
             .catch((err) => {
                 console.log(err)
@@ -182,6 +145,7 @@ const AddPost = ({src, baseUrl, userImg}) => {
                 file : img,
                 hashtag : hashtagArr
             };
+            console.log(formdata);
 
             form.append('content', formdata.content);
             form.append('img', formdata.file[0]);
@@ -196,7 +160,7 @@ const AddPost = ({src, baseUrl, userImg}) => {
                 console.log(res)
                 setTimeout(function() {
                     window.location.reload();
-                }, 300);
+                }, 200);
             })
             .catch((err) => {
                 console.log(err)
@@ -222,24 +186,20 @@ const AddPost = ({src, baseUrl, userImg}) => {
         recPreview = <S.previewIcon src={sound}></S.previewIcon>
     }
     
-
     return (
         <S.mainContainer>
             <S.mainSection>
                 <S.writerInfoBox>
                     <S.profileImg src={src + userImg}></S.profileImg>
                     <S.form action="" method="post" enctype="multipart/form-data" input>
-                        <S.writerInput placeholder="이야기를 들려주세요!" type="text" name="content" ref={postInput}>
+                        <S.writerInput placeholder="이야기를 들려주세요!" type="text" name="content">
                         </S.writerInput>
                     </S.form>
                     </S.writerInfoBox>
                     <S.buttonsContainer container>
                         <S.buttonsContainer>
                             <S.form>
-                                <S.buttonBox onClick={onRec? onRecAudio : offRecAudio}>
-                                    <S.buttonIcon src={mic}></S.buttonIcon>
-                                    녹음
-                                </S.buttonBox>
+                                <AudioRecord setRecord={setRecord} setTimer={setTimer}></AudioRecord>
                             </S.form>
                             <S.form enctype="multipart/form-data">
                                 <S.buttonBox for="audioFile">
@@ -258,7 +218,7 @@ const AddPost = ({src, baseUrl, userImg}) => {
                             {recPreview}
                             {imgPreview}
                         </S.buttonsContainer>
-                        
+                        <R.ListeningButton onClick={onEditPost}>수정</R.ListeningButton>
                         <R.ListeningButton onClick={onAddPost}>업로드</R.ListeningButton>
                     </S.buttonsContainer>
                     <S.buttonsContainer rec>
