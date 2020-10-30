d82795ca747b6def696503b643881ddb77b8105c
Srim <deamoon0801@naver.com>
2020-10-31 03:56:33 +0900

    [MODIFY] code refactor

--- yally/src/components/Main/PostItem.js
+++ yally/src/components/Main/PostItem.js
@@ -6,8 +6,8 @@ import { Link } from "react-router-dom";
 import axios from 'axios';
 import yallyOn from '../../assets/img/yallyOn.png';
 import yallyOff from '../../assets/img/yallyOff.png';
-import AudioPlayer from './AudioPlayer';
 import Modal from '../Global/Modal';
+import AudioPlayer from './AudioPlayer';
 
 const PostItem = ({email, baseUrl, id, date, nickname, isYally, yallyNum, isComment, content, sound, isMine, userImg, audioImg, setContent}) => {
     
@@ -24,19 +24,15 @@ const PostItem = ({email, baseUrl, id, date, nickname, isYally, yallyNum, isComm
     }
 
     const [onLike, setOnLike] = useState(yallySrc);
-    const [onAudio, setOnAudio] = useState(true);
-    // const audio = new Audio(src + sound);
-    const audio = document.getElementById('audio');
-
     let deleteButtonStyle = "";
     const config = {
         headers : { 'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8'}
     }
     if(isMine === false)
     {
-        deleteButtonStyle = "none"
+        deleteButtonStyle = "none";
     }
-
+    
     const onYally = () => {
         if(isYally === false)
         {
@@ -60,18 +56,6 @@ const PostItem = ({email, baseUrl, id, date, nickname, isYally, yallyNum, isComm
     const onEditPost = () => {
         setContent(content, sound, audioImg, id);
         window.scrollTo({top:0, left:0, behavior:'smooth'});
-    }
-
-    const onAudioPlay = () => {
-        audio.play();
-        console.log('hi', sound);
-        setOnAudio(false);
-    }
-
-    const offAudioPlay = () => {
-        audio.pause();
-        setOnAudio(true);
-        console.log('bye', sound);
     }
 
     let createdDate = date.split('-');
@@ -113,9 +97,7 @@ const PostItem = ({email, baseUrl, id, date, nickname, isYally, yallyNum, isComm
                             <P.postArticle>
                                 <P.postWritten>{content}</P.postWritten>
                                 <P.playInfoBox>
-                                    {/* <AudioPlayer src={src + sound}></AudioPlayer> */}
-                                    <audio src={src + sound} controls id="audio"></audio>
-                                    <P.Icon src={playButton} onClick={onAudio? onAudioPlay : offAudioPlay}></P.Icon>
+                                    <AudioPlayer audio={src + sound}></AudioPlayer>
                                 </P.playInfoBox>
                             </P.postArticle>
                         </P.postInfoContainer>
