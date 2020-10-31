2c55fb0a718b3c6dbeb7b238cccee7862699706d
Srim <deamoon0801@naver.com>
2020-10-31 17:38:14 +0900

    [ADD] Listening

--- yally/src/components/Main/Recommended.js
+++ yally/src/components/Main/Recommended.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from "../../assets/style/Main/AddTimeLine";
 import * as R from '../../assets/style/Main/Recommend';
 import axios from 'axios'
 
 const Recommended = ({src, nickname, id, userImg, baseUrl, email}) => {
+    const [onListen, setOnListen] = useState(false);
+
     const onListening = () => {
         const config = {
             headers : { 'Authorization' : 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MDEzNTAyNzUsIm5iZiI6MTYwMTM1MDI3NSwianRpIjoiNjM1ZTk3OWItNjczZC00ZmI5LTg3MmEtZDE2MjdjNGQyYTBlIiwiZXhwIjoxNjA5OTkwMjc1LCJpZGVudGl0eSI6ImFkbWluQGdtYWlsLmNvbSIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.3fLkBFWZ9N0Cq0xGEXZzVeKjNvkqkVdREsMOJwbtzy8'}
@@ -13,6 +15,7 @@ const Recommended = ({src, nickname, id, userImg, baseUrl, email}) => {
         axios.post(baseUrl + 'user/listening' , data, config)
         .then((res) => {
             console.log(res);
+            setOnListen(true);
         })
         .catch((err) => {
             console.log(err);
@@ -20,7 +23,7 @@ const Recommended = ({src, nickname, id, userImg, baseUrl, email}) => {
     }
 
     return (
-            <R.recommendBox>
+            <R.recommendBox style={{display : onListen? "none" : ""}}>
                 <S.profileImg src={src + userImg}></S.profileImg>
                 <R.userName>{nickname}</R.userName>
                 <R.ListeningButton onClick={onListening}>리스닝</R.ListeningButton>
