dbec2120971ca2b20382f2c3ba691be4507b4ab7
Haerin <kim34458747@gmail.com>
2020-11-06 23:11:13 +0900

    App.js 수정

--- yally/src/App.js
+++ yally/src/App.js
@@ -2,17 +2,21 @@ import React, { useCallback } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import DetailPostView from "./components/PostDetail/DetailPostView";
 import TimeLineView from "./components/Main/TimeLineView";
+import Setting from "./components/AccountSetting/Setting";
+import Profile from "./components/UserPage/Profile";
+import SignUp from "./components/SignUp/SignUp";
+import { url } from "./constant";
+import Login from "./components/Login/Login";
+import SignUpCheck from "./components/SignUp/SignUpCheck";
 import Users from "./components/Search/Users";
 import PostItem from "./components/Main/PostItem";
 import Header from "./components/Header/Header";
-import Setting from "./components/AccountSetting/Setting";
 import Background from "./components/Global/Background";
-import Profile from "./components/UserPage/Profile";
 import Listener from "./components/Listen/Listener";
 import Listening from "./components/Listen/Listening";
 
 function App() {
-  const baseUrl = "http://13.125.238.84:81/";
+  const baseUrl = useCallback(url);
   const src = "https://yally-sinagram.s3.ap-northeast-2.amazonaws.com/";
 
   return (
@@ -39,12 +43,30 @@ function App() {
             )}
           />
           <Route
-            path="/profile/:email/listener/:value/"
+            exact
+            path="/profile/settings"
+            render={() => <Setting baseUrl={baseUrl} />}
+          />
+          <Route
+            exact
+            path="/profile/:name/listener/:value/"
             render={(match) => <Listener baseUrl={baseUrl} match={match} />}
           />
           <Route
-            path="/profile/:email/listening/:value/"
+            exact
+            path="/profile/:name/listening/:value/"
             render={(match) => <Listening baseUrl={baseUrl} match={match} />}
+          />
+          <Route
+            exact
+            path="/sign-up"
+            render={() => <SignUp baseUrl={baseUrl} />}
+          />
+          <Route exact path="/" render={() => <Login baseUrl={baseUrl} />} />
+          <Route
+            exact
+            path="/sign-up-check"
+            render={() => <SignUpCheck baseUrl={baseUrl} />}
           />
           <Route exact path="/search/users" render={() => <Users />} />
           <Route exact path="/search/posts" render={() => <PostItem />} />
