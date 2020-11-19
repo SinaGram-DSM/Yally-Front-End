55f1f9205f5bd8ce283de8c3b8db5397bf317593
Srim <deamoon0801@naver.com>
2020-11-19 17:09:43 +0900

    [STYLE] <TimeLine> style

--- yally/src/components/Main/RecommendView.js
+++ yally/src/components/Main/RecommendView.js
@@ -19,6 +19,7 @@ const RecommendView = ({src, baseUrl}) => {
                 refresh();
             }
         })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
