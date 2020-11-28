07a44c5c7e641b98722dc76ef54593f31baa0b3d
Haerin <kim34458747@gmail.com>
2020-11-29 01:15:23 +0900

    listen

--- yally/src/components/Search/Users.js
+++ yally/src/components/Search/Users.js
@@ -1,17 +1,57 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useLocation } from "react-router";
+import axios from "axios";
 import * as T from "../../assets/style/UserPage/Listen";
 import * as L from "../../assets/style/UserPage/PageStyle";
 
-const Users = ({ img, nickname, listening, listener, isListening }) => {
+const Users = ({
+  id,
+  email,
+  img,
+  nickname,
+  listening,
+  listener,
+  isListening,
+  baseUrl,
+}) => {
   const url = "https://yally-sinagram.s3.ap-northeast-2.amazonaws.com/";
+  const history = new useHistory();
+  const location = useLocation;
+  let [isListen, setIsListen] = useState(isListening);
   let [loading, setLoading] = useState(false);
   useEffect(() => {
     if (img != undefined) setLoading(true);
+    setIsListen(isListening);
   }, []);
+  const config = {
+    headers: {
+      Authorization: "Bearer " + localStorage.getItem("accessToken"),
+    },
+  };
+
+  const userListening = () => {
+    axios
+      .post(baseUrl + "user/listening/" + email, null, config)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.message == "Success") setIsListen(true);
+      });
+  };
+
+  const userUnListening = () => {
+    axios.delete(baseUrl + "user/listening/" + email, config).then((res) => {
+      if (res.data.message == "Success") setIsListen(false);
+    });
+  };
+
+  const profile = () => {
+    history.push(`/profile/${email}`);
+  };
   return (
     <T.containerBox load={loading}>
       <T.itemBox>
-        <T.boxImg src={url + img} />
+        <T.boxImg src={url + img} onClick={profile} />
         <T.userBox>
           <T.name>{nickname}</T.name>
           <L.Listen list>
@@ -20,10 +60,10 @@ const Users = ({ img, nickname, listening, listener, isListening }) => {
           </L.Listen>
         </T.userBox>
       </T.itemBox>
-      {isListening ? (
-        <T.listenBtn>리스닝</T.listenBtn>
+      {isListen ? (
+        <T.unlistenBtn onClick={userUnListening}>언리스닝</T.unlistenBtn>
       ) : (
-        <T.unlistenBtn>언리스닝</T.unlistenBtn>
+        <T.listenBtn onClick={userListening}>리스닝</T.listenBtn>
       )}
     </T.containerBox>
   );
