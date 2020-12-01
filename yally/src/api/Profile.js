092e508e619b814337fee3cf2de4b449cacc3184
Srim <deamoon0801@naver.com>
2020-12-01 17:19:27 +0900

    [ADD] <api/Profile.js> api

--- yally/src/api/Profile.js
+++ yally/src/api/Profile.js
@@ -1,1 +1,13 @@
-import { getClientAccessToken } from "./client";
+import { getClientBearerAccessToken, getClientAccessToken } from "./client";
+
+export const getProfile = (email) => {
+  return getClientAccessToken.get(`/profile/${email}`);
+};
+
+export const getMypage = (email, page) => {
+  return getClientBearerAccessToken.get(`/mypage/timeline/${email}/${page}`);
+};
+
+export const editProfile = (form) => {
+  return getClientAccessToken.put(`/profile/`, form);
+};
