26808f3c914eac37f18dea1432ea004a2290dd58
Srim <deamoon0801@naver.com>
2020-12-01 17:19:38 +0900

    [ADD] <api/search.js> api

--- yally/src/api/Search.js
+++ yally/src/api/Search.js
@@ -1,1 +1,13 @@
-import { getClientAccessToken } from "./client";
+import { getClientBearerAccessToken } from "./client";
+
+export const tagSearch = (values, page) => {
+  return getClientBearerAccessToken.get(
+    `/search/post?hashtag=${values}&page=${page}`
+  );
+};
+
+export const userSearch = (values, page) => {
+  return getClientBearerAccessToken.get(
+    `/search/user?nickname=${values}&page=${page}`
+  );
+};
