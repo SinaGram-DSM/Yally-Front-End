810081a6fa14e3eb274f975fb53fe0864ee32ff8
Srim <deamoon0801@naver.com>
2020-12-01 12:02:05 +0900

    [STYLE] <components/Search/Users.js> useLocation

--- yally/src/components/Search/Users.js
+++ yally/src/components/Search/Users.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useHistory } from "react-router-dom";
-import { useLocation } from "react-router";
+import { useHistory, useLocation } from "react-router-dom";
 import axios from "axios";
 import * as T from "../../assets/style/UserPage/Listen";
 import * as L from "../../assets/style/UserPage/PageStyle";
