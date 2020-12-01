d361db9fd8e3ba1e3020824ff74f8e4f5afaef9b
Srim <deamoon0801@naver.com>
2020-12-01 17:18:07 +0900

    [MODIFY] <App.js>

--- yally/src/App.js
+++ yally/src/App.js
@@ -1,31 +1,29 @@
-import React, { useCallback, useState} from "react";
+import React, { useState} from "react";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
-import { url } from "./constant/index";
 import { DetailPostView, TimeLineView, Setting, Profile, SignUp, Login, SignUpCheck, Users, PostItem, Listener, Listening, Header } from "./components/index";
 
 function App() {
   const [isToken] = useState(localStorage.getItem("accessToken"));
-  const baseUrl = useCallback(url);
 
   return (
     <div style={{ position: "relative", backgroundColor: "#FDFDFD" }}>
       <Router>
-        <Header baseUrl={baseUrl} />
+        <Header />
         <Switch>
           <Route exact path="/timeline" render={() => <TimeLineView />} />
           <Route exact path="/post/:id" render={() => (<DetailPostView/>)} />
-          <Route exact path="/profile/settings" render={() => <Setting baseUrl={baseUrl} />}/>
-          <Route exact path="/profile/:email/listener/:value/" render={(match) => <Listener baseUrl={baseUrl} match={match} />}/>
-          <Route exact path="/profile/:email/listening/:value/" render={(match) => <Listening baseUrl={baseUrl} match={match} />}/>
+          <Route exact path="/profile/settings" render={() => <Setting />}/>
+          <Route exact path="/profile/:email/listener/:value/" render={(match) => <Listener match={match} />}/>
+          <Route exact path="/profile/:email/listening/:value/" render={(match) => <Listening match={match} />}/>
           <Route exact path="/sign-up" render={() => <SignUp />}/>
           <Route exact path="/" render={() => <Login />}>
           {isToken ? <Redirect to="/timeline" /> : <Login />}
           </Route>
           <Route exact path="/sign-up-check" render={() => <SignUpCheck />}/>
-          <Route exact path="/search/users" render={() => <Users baseUrl={baseUrl} />} />
+          <Route exact path="/search/users" render={() => <Users />} />
           <Route exact path="/search/posts" render={() => <PostItem />} />
-          <Route exact path="/settings" render={() => <Setting baseUrl={baseUrl} />} />
-          <Route exact path="/profile/:email" render={(props) => <Profile props={props} baseUrl={baseUrl}/>} />
+          <Route exact path="/settings" render={() => <Setting />} />
+          <Route exact path="/profile/:email" render={(props) => <Profile props={props} />} />
         </Switch>
       </Router>
     </div>
