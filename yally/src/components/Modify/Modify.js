a196fc6dc11fa47079bae0e15b05cf5d9708412f
Srim <deamoon0801@naver.com>
2020-12-20 23:34:52 +0900

    [ADD] <Modify.js> component

---
+++ yally/src/components/Modify/Modify.js
@@ -0,0 +1,222 @@
+import React,{ useState, useEffect } from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+import * as S from "../../assets/style/Main/AddTimeLine";
+import * as R from '../../assets/style/Main/Recommend';
+import { sound, picture } from '../../assets/img';
+import AudioRecord from '../Main/AudioRecord'
+import '../../assets/style/Global/global.css';
+import { Link } from 'react-router-dom';
+import { getTimelineInfo } from '../../lib/api/timeline';
+import { editPost } from '../../lib/api/post';
+import { refreshToken } from '../../lib/api/user';
+
+const Modify = ({ editContent, editFile, editImg }) => {
+
+    const [audioUrl, setAudioUrl] = useState();
+    const [previewUrl, setPreviewUrl] = useState('');
+    const [imgFile, setImgFile] = useState('');
+    const [isOnAudio, setIsOnAudio] = useState(false);
+    const [onRecText, setOnRecText] = useState(null);
+    const [start, setStart] = useState();
+    const [onText, setOnText] = useState();
+    const [audioStart, setAudioStart] = useState(true);
+    const [isOnImg, setIsOnImg] = useState(false);
+    const [content, setContent] = useState();
+    const [isContent, setIsContent] = useState(false);
+    const [user, setUser] = useState({});
+    const [email, setEmail] = useState({});
+    let imgPreview = null;
+    let recPreview = null;
+    const history = useHistory();
+    const location = useLocation();
+    const id = location.pathname.split("/");
+
+    const setRecord = (audio, onAudio) => {
+        setAudioUrl(audio);
+        setIsOnAudio(onAudio);
+    }
+
+    useEffect(() => {
+        getTimelineInfo()
+        .then((res) => {
+            setUser(res.data.info);
+            setEmail(res.data.info.email);
+        })
+    }, [])
+
+    const startTimer = () => {
+        let rsec = 1;
+        let lsec = 0;
+        let rmin = 0;
+        let timer = setInterval(() => {
+            setOnText(<S.buttonsContainer><S.recordingIcon></S.recordingIcon><S.recordText>{'0' + rmin + ' : ' + lsec + rsec}</S.recordText></S.buttonsContainer>);
+            setOnRecText(<S.recordText>음성 녹음 버튼을 한 번 더 누르면 녹음이 종료됩니다.</S.recordText>);
+            rsec++;
+            if(rsec > 9) 
+            {
+                lsec++;
+                rsec = 0;
+            }
+            if(lsec > 5) 
+            {
+                lsec = 0;
+                rmin++;
+            }
+        }, 1000)
+        setStart(timer);
+        setAudioStart(false);
+    }
+
+    const endTimer = () => {
+        setOnText(null);
+        setOnRecText(null);
+        clearInterval(start);
+    }
+    
+    const onUploadRec = () => {
+        let audioFile = document.getElementById('audioFile').files;
+        setAudioUrl(audioFile[0]);
+        setIsOnAudio(true);
+    }
+
+    const onEditPost = () => {
+        let editFileArr = [];
+        let editImgArr = [];
+        editFileArr.push(editFile);
+        editImgArr.push(editImg);
+        const editSound = new File([editFileArr], "sound", { lastModified: new Date().getTime(), type: 'audio/mp3' });
+        const editImgFile = new File([editImgArr], "img", { lastModified: new Date().getTime(), type: "image/jpeg" });
+
+        let editHashtagArr = [];
+        let editHashtag = '';
+        editHashtagArr = editContent.split('#');
+        for(let i = 1; i < editHashtagArr.length; i++)
+        {
+            editHashtag = editHashtagArr[i];
+        }
+            
+        editHashtag = editHashtag.split(' ');
+        editHashtagArr = editHashtag;
+
+       console.log(editSound, editImgFile)
+        const editFormData = {
+            sound : editSound,
+            content : editContent,
+            hashtags : editHashtagArr
+        };
+
+        let editForm = new FormData();
+
+        if(isContent === true) {
+            editForm.append('content', content);
+        }
+        else {
+            editForm.append('content', editContent);
+        }
+
+        if(isOnImg === true) {
+            editForm.append('img', imgFile);
+        } 
+        else {
+            editForm.append('img', editImgFile);
+        }
+
+        if(isOnAudio === true) {
+            editForm.append('sound', audioUrl);
+        }
+        else {
+            editForm.append('sound', editSound);
+        }
+        
+        editForm.append('hashtag', editFormData.hashtags);
+
+        editPost(id[2], editForm)
+            .then((res) => {
+                console.log(res);
+                setTimeout(function() {
+                    history.push({
+                        pathname : "/timeline"
+                    })
+                }, 200);
+            })
+            .catch((err) => {
+                alert('글 수정에 실패하였습니다. 오디오를 입력해주세요.');
+                if(err.status === 403) {
+                    refreshToken();
+                }
+            })
+    }
+
+    const onUploadImg = (e) => {
+        e.preventDefault();
+        let reader = new FileReader();
+        let file = e.target.files[0];
+        reader.onloadend = () => {
+            setPreviewUrl(reader.result);
+            setImgFile(file);
+            setIsOnImg(true);
+        }
+        reader.readAsDataURL(file);
+    }
+
+    const onUploadContent = (e) => {
+        e.preventDefault();
+        setContent(e.target.value);
+        setIsContent(true);
+    }
+
+    if(imgFile !== ''){
+      imgPreview = <S.previewIcon src={previewUrl}></S.previewIcon>
+    }
+    if(isOnAudio === true){
+        recPreview = <S.previewIcon src={sound}></S.previewIcon>
+    }
+    
+    return (
+        <S.mainContainer>
+            <S.mainSection>
+                <S.writerInfoBox>
+                <Link style={{textDecoration : "none"}} to={{
+                pathname : `/profile/${user.email}`,
+                state : {
+                    email
+                }
+                }}><S.profileImg src={process.env.REACT_APP_SRC_URL + user.img}></S.profileImg></Link>
+                    <S.form action="" method="post" enctype="multipart/form-data" input>
+                        <S.writerInput placeholder={`${user.nickname} 님의 이야기를 들려주세요!`} type="text" name="content" onChange={onUploadContent}></S.writerInput>
+                    </S.form>
+                    </S.writerInfoBox>
+                    <S.buttonsContainer container>
+                        <S.buttonsContainer>
+                            <S.form onClick={audioStart? startTimer : endTimer}>
+                                <AudioRecord setRecord={setRecord}></AudioRecord>
+                            </S.form>
+                            <S.form enctype="multipart/form-data">
+                                <S.buttonBox for="audioFile">
+                                    <S.inputFile  type="file" id="audioFile" accept="audio/*" capture="microphone" onChange={onUploadRec}/>
+                                    <S.buttonIcon src={sound}></S.buttonIcon>
+                                    음성 파일
+                                </S.buttonBox>
+                            </S.form>
+                            <S.form method="post" enctype="multipart/form-data">
+                                <S.buttonBox for="audioImg">
+                                    <S.inputFile type="file" id="audioImg" accept="image/*" onChange={onUploadImg}/>
+                                    <S.buttonIcon src={picture}></S.buttonIcon>
+                                    음성 커버
+                                </S.buttonBox>
+                            </S.form>
+                            {recPreview}
+                            {imgPreview}
+                        </S.buttonsContainer>
+                        <R.ListeningButton onClick={onEditPost}>수정</R.ListeningButton>
+                    </S.buttonsContainer>
+                    <S.buttonsContainer rec>
+                        {onText}
+                        {onRecText}
+                    </S.buttonsContainer>
+            </S.mainSection>
+        </S.mainContainer>
+    );
+};
+
+export default Modify;
