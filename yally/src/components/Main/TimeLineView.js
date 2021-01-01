6cf11e6e31e4d3ce38c826e330bc88eaebfd645c
Srim <deamoon0801@naver.com>
2021-01-01 21:28:04 +0900

    [STYLE] code style

--- yally/src/components/Main/TimeLineView.js
+++ yally/src/components/Main/TimeLineView.js
@@ -7,7 +7,7 @@ import NotFound from "./NotFound";
 import Loader from "./Loader";
 import { getTimeline } from "../../lib/api/timeline";
 import { refreshToken } from "../../lib/api/user";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 import Header from "../Header/Header";
 
 const TimeLineView = () => {
@@ -31,54 +31,56 @@ const TimeLineView = () => {
   const [posts, setPosts] = useState([]);
   const [params, setParams] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
-  
+
   const infiniteScroll = useCallback(() => {
-    let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
-    let scrollTop = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+    let scrollHeight = Math.max(
+      document.documentElement.scrollHeight,
+      document.body.scrollHeight
+    );
+    let scrollTop = Math.max(
+      document.documentElement.scrollTop,
+      document.body.scrollTop
+    );
     let clientHeight = document.documentElement.clientHeight;
 
     if (scrollTop + clientHeight === scrollHeight && isLoading === true) {
       setIsScroll(true);
       setTimeout(function () {
-        setPosts(posts.concat(posts));
+        setPosts((state) => state.concat(state));
         setParams((state) => state + 1);
         setIsLoading(false);
         console.log(params);
       }, 500);
     }
-  }, [isLoading, posts, params]);
+  }, [isLoading, params]);
 
   useEffect(() => {
-    getTimeline(params).then((res) => {
-      if(res.data.posts === '') {
-        setNotPosts("더 이상 글이 없어요. 더 작성해보세요!");
-        setIsScroll(false);
-      }
-      else if(posts === '' && res.data.posts === '') {
-        setNotFound(false);
-      }
-      else {
-        setNotFound(false);
-        setPosts(res.data.posts);
-        setIsLoading(true);
-        setIsScroll(false);
-      }
-      
-    }).catch((err) => {
-            if(err.status === 403) {
-                refreshToken();
-            }
-            else {
-              if(err.status)
-              {
-                setStatusCode(err.status);
-              }
-              else setStatusCode("ERROR");
-            }
-        })
+    getTimeline(params)
+      .then((res) => {
+        if (res.data.posts === "") {
+          setNotPosts("더 이상 글이 없어요. 더 작성해보세요!");
+          setIsScroll(false);
+        } else if (posts === "" && res.data.posts === "") {
+          setNotFound(false);
+        } else {
+          setNotFound(false);
+          setPosts(res.data.posts);
+          setIsLoading(true);
+          setIsScroll(false);
+        }
+      })
+      .catch((err) => {
+        if (err.status === 403) {
+          refreshToken();
+        } else {
+          if (err.status) {
+            setStatusCode(err.status);
+          } else setStatusCode("ERROR");
+        }
+      });
     window.addEventListener("scroll", infiniteScroll);
     return () => window.removeEventListener("scroll", infiniteScroll);
-  }, [infiniteScroll, posts, params]);
+  }, [infiniteScroll]);
 
   return (
     <div
@@ -87,33 +89,42 @@ const TimeLineView = () => {
     >
       <Header />
       <ToastContainer />
-     { notFound ? (<NotFound status={statusCode}/>) : (<div><AddPost
-        editContent={contents}
-        editFile={file}
-        editImg={img}
-        editPostId={postId}
-      ></AddPost>
-      <RecommendView />
-      {posts.map((post) => (
-        <PostItem
-          email={post.user.email}
-          key={post.id}
-          id={post.id}
-          content={post.content}
-          sound={post.sound}
-          date={post.createdAt}
-          nickname={post.user.nickname}
-          userImg={post.user.img}
-          audioImg={post.img}
-          isComment={post.comment}
-          isYally={post.isYally}
-          yallyNum={post.yally}
-          isMine={post.isMine}
-          setContent={setContent}
-        ></PostItem>
-      ))}
-        <h2 style={{textAlign : "center", color : "#707070"}}>{isScroll? <Loader></Loader> : ""}{notPosts}</h2></div>)}
-        <Background></Background>
+      {notFound ? (
+        <NotFound status={statusCode} />
+      ) : (
+        <div>
+          <AddPost
+            editContent={contents}
+            editFile={file}
+            editImg={img}
+            editPostId={postId}
+          ></AddPost>
+          <RecommendView />
+          {posts.map((post) => (
+            <PostItem
+              email={post.user.email}
+              key={post.id}
+              id={post.id}
+              content={post.content}
+              sound={post.sound}
+              date={post.createdAt}
+              nickname={post.user.nickname}
+              userImg={post.user.img}
+              audioImg={post.img}
+              isComment={post.comment}
+              isYally={post.isYally}
+              yallyNum={post.yally}
+              isMine={post.isMine}
+              setContent={setContent}
+            ></PostItem>
+          ))}
+          <h2 style={{ textAlign: "center", color: "#707070" }}>
+            {isScroll ? <Loader></Loader> : ""}
+            {notPosts}
+          </h2>
+        </div>
+      )}
+      <Background></Background>
     </div>
   );
 };
